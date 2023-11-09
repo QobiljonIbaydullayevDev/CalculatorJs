460f229abeef4c3768272ba1fef9c2f622ced00d
QobiljonIbaydullayevDev <qobiljonyoqubovich2002@gmail.com>
2023-11-09 14:04:59 +0500

    .

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ const AC = document.querySelector('.operator');
 
 const display = document.querySelector(".display")
 
-const clickHandler =() =>{
+const persentage = document.getElementById("persentage")
+
+const division = document.getElementById("division")
+
+const clearHandler =() =>{
     display.value = ""
+}
+
+const persentageHandler=()=>{
+    console.log(display.value);
+    display.value= display.value/100
+}
+
+const operatorHandler=()=>{
+    
 }
